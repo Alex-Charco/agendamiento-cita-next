f447476cf44e059822d6f009bc20e1cc892775e1
Alex-Charco <awcharco@utpl.edu.ec>
2025-03-26 20:54:06 -0500

    Eliminación de código comentado

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -17,23 +17,3 @@ const Button = ({ text, onClick, variant = "primary" }) => {
 };
 
 export default Button;
-
-
-
-
-/*"use client";
-import { Button as HeroButton } from "@heroui/react";
-
-const Button = ({ text, onClick }) => {
-  return (
-    <HeroButton
-      onClick={onClick}
-      className="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold shadow-md hover:bg-blue-700 transition"
-    >
-      {text}
-    </HeroButton>
-  );
-};
-
-export default Button;
-*/
