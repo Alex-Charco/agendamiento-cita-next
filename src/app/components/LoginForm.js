e9a67d2d343197d1e53e1097e7d0c9bbcb4c8541
Alex-Charco <awcharco@utpl.edu.ec>
2025-03-29 14:26:40 -0500

    Se cambia diseño imagen login y cambio de logo

--- src/app/components/LoginForm.js
+++ src/app/components/LoginForm.js
@@ -1,7 +1,8 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Button, Input, Card, CardHeader, CardBody, Image } from "@heroui/react";
+import { Button, Input, Card, CardHeader, CardBody } from "@heroui/react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
@@ -32,7 +33,7 @@ export default function LoginForm() {
     const onSubmit = async (e) => {
         e.preventDefault();
         const data = Object.fromEntries(new FormData(e.currentTarget));
-        
+
         const nombre_usuario = data.nombre_usuario;
         const password = data.password;
 
@@ -45,9 +46,9 @@ export default function LoginForm() {
             const response = await axios.post(
                 `${API_BASE_URL}/api/auth/login`,
                 { nombre_usuario, password },
-                { 
+                {
                     headers: { "Content-Type": "application/json" },
-                    withCredentials: true 
+                    withCredentials: true,
                 }
             );
 
@@ -80,68 +81,109 @@ export default function LoginForm() {
             }
         } catch (error) {
             setErrorMessage("Error en la autenticación. Verifica tus datos.");
-            console.error("Error en la petición:", error.response?.data || error.message);
+            console.error(
+                "Error en la petición:",
+                error.response?.data || error.message
+            );
         }
     };
 
     return (
-        <div className="flex h-screen items-center justify-center bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: "url('/images/background.svg')" }}>
+        <div
+            className="flex h-screen items-center justify-center bg-cover bg-center bg-no-repeat p-4"
+            style={{ backgroundImage: "url('/images/background.svg')" }}
+        >
             <div className="absolute inset-0 bg-black bg-opacity-5"></div>
 
-            <div className="relative z-10">
-                <Card className="max-w-md w-full bg-gradient-to-b from-celeste-fuerte to-[#F5F7FC] bg-opacity-50"
+            <div className="relative z-10 w-full max-w-md">
+                <Card className="max-w-lg w-full bg-gradient-to-b from-celeste-fuerte to-[#F5F7FC] bg-opacity-50"
                     style={{
-                        border: '4px solid rgba(0, 56, 255, 0.3)',
-                        boxShadow: '0 4px 10px rgba(0, 56, 255, 0.5)',
-                    }}>
+                        border: "4px solid rgba(0, 56, 255, 0.3)",
+                        boxShadow: "0 4px 10px rgba(0, 56, 255, 0.5)",
+                    }}
+                >
                     <CardHeader className="text-center p-0 mb-6">
-                        <div className="mb-0">
-                            <Image src="/images/logo.png" alt="Logo" className="w-full h-auto" />
-                        </div>
-                    </CardHeader>
+						<div className="relative w-full h-[12rem]"> {/* Ajusta la altura según necesites */}
+							<Image
+								src="/images/logo.png"
+								alt="Logo"
+								fill
+								className="object-cover"
+								priority
+							/>
+						</div>
 
+					</CardHeader>
                     <CardBody>
                         <form className="flex flex-col gap-4" onSubmit={onSubmit}>
                             <div className="flex flex-col gap-4">
                                 <div className="flex items-center w-full">
-                                    <label className="text-sm text-gray-700 w-1/3 text-left" htmlFor="nombre_usuario">
+                                    <label
+                                        className="text-sm text-gray-700 w-1/3 text-left"
+                                        htmlFor="nombre_usuario"
+                                    >
                                         Usuario: *
                                     </label>
-                                    <Input isRequired id="nombre_usuario" name="nombre_usuario"
-                                        placeholder="Ingresar el usuario" className="w-full md:w-3/4" />
+                                    <Input
+                                        isRequired
+                                        id="nombre_usuario"
+                                        name="nombre_usuario"
+                                        placeholder="Ingresar el usuario"
+                                        className="w-full"
+                                    />
                                 </div>
 
                                 <div className="flex items-center w-full">
-                                    <label className="text-sm text-gray-700 w-1/3 text-left" htmlFor="password">
+                                    <label
+                                        className="text-sm text-gray-700 w-1/3 text-left"
+                                        htmlFor="password"
+                                    >
                                         Contraseña: *
                                     </label>
-                                    <div className="relative w-full md:w-3/4">
-                                        <Input isRequired id="password" name="password"
-                                            placeholder="Ingresar la contraseña" type={isVisible ? "text" : "password"} className="w-full" />
-                                        <button aria-label="toggle password visibility"
+                                    <div className="relative w-full">
+                                        <Input
+                                            isRequired
+                                            id="password"
+                                            name="password"
+                                            placeholder="Ingresar la contraseña"
+                                            type={isVisible ? "text" : "password"}
+                                            className="w-full"
+                                        />
+                                        <button
+                                            aria-label="toggle password visibility"
                                             className="absolute right-3 top-1/2 transform -translate-y-1/2 focus:outline-none"
-                                            type="button" onClick={toggleVisibility}>
-                                            {isVisible ? <FaEye className="text-2xl text-gray-500" /> : <FaEyeSlash className="text-2xl text-gray-500" />}
+                                            type="button"
+                                            onClick={toggleVisibility}
+                                        >
+                                            {isVisible ? (
+                                                <FaEye className="text-2xl text-gray-500" />
+                                            ) : (
+                                                <FaEyeSlash className="text-2xl text-gray-500" />
+                                            )}
                                         </button>
                                     </div>
                                 </div>
                             </div>
 
                             <div className="flex flex-col justify-center w-full">
-								<Button className="mx-auto w-1/3" type="submit" color="primary">
-									Ingresar
-								</Button>
-								<p className="flex justify-center text-xs text-gray-700 mt-2">
-									Olvidó la contraseña:{" "}
-									<a href="/auth/reset-password" className="text-blue-600 hover:underline">
-										Recuperar contraseña
-									</a>
-								</p>
-							</div>
+                                <Button className="mx-auto w-1/3" type="submit" color="primary">
+                                    Ingresar
+                                </Button>
+                                <p className="flex justify-center text-xs text-gray-700 mt-2">
+                                    Olvidó la contraseña:{" "}
+                                    <a
+                                        href="/auth/reset-password"
+                                        className="text-blue-600 hover:underline"
+                                    >
+                                        Recuperar contraseña
+                                    </a>
+                                </p>
+                            </div>
                         </form>
 
-                        {errorMessage && <div className="text-red-500 mt-2">{errorMessage}</div>}
+                        {errorMessage && (
+                            <div className="text-red-500 mt-2">{errorMessage}</div>
+                        )}
                         <p className="text-sm text-center font-bold text-blue-700 mt-4 border-t border-plomo-claro pt-2 w-full mx-0">
                             Sistema de Gestión Hospitalaria
                         </p>
@@ -151,9 +193,6 @@ export default function LoginForm() {
         </div>
     );
 }
-
-
-
 
 /*"use client";
 
