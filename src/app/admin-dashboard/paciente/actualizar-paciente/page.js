7800f98bf2f0c0add582d50fba87057e83a307a1
Alex-Charco <awcharco@utpl.edu.ec>
2025-04-07 16:01:43 -0500

    Eliminación de codigo sin uso

--- src/app/admin-dashboard/paciente/actualizar-paciente/page.js
+++ src/app/admin-dashboard/paciente/actualizar-paciente/page.js
@@ -1,6 +1,6 @@
 "use client";
+
 import { useState } from "react";
-import axios from "axios";
 import { FaSearch, FaTimes, FaSignOutAlt } from "react-icons/fa";
 import NavbarComponent from "@/admin-dashboard/paciente/components/NavbarComponent";
 import CustomTabs from "@/components/CustomTabs";
@@ -17,110 +17,106 @@ import ReusableModal from "@/components/ReusableModal";
 import { useDisclosure } from "@heroui/react";
 
 export default function ActualizarPacientePage() {
-    const [query, setQuery] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [selectedPaciente, setSelectedPaciente] = useState(null);
-    const [selectedFamiliar, setSelectedFamiliar] = useState(null);
-    const [selectedInfoMilitar, setSelectedInfoMilitar] = useState(null);
-    const [selectedResidencia, setSelectedResidencia] = useState(null);
-    const [selectedSeguro, setSelectedSeguro] = useState(null);
+	const [selectedPaciente, setSelectedPaciente] = useState(null);
+	const [selectedFamiliar, setSelectedFamiliar] = useState(null);
+	const [selectedInfoMilitar, setSelectedInfoMilitar] = useState(null);
+	const [selectedResidencia, setSelectedResidencia] = useState(null);
+	const [selectedSeguro, setSelectedSeguro] = useState(null);
 	const [selectedUsuario, setSelectedUsuario] = useState(null);
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-    const [activeTab, setActiveTab] = useState("datos-generales");
+	const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
 	const handleUsuarioSelect = (usuario) => {
-	  const datosMapeados = {
-		nombre_usuario: usuario.nombre_usuario,
-		estatus: usuario.estatus ?? "",
-		nombre_rol: usuario.rol?.nombre_rol ?? "",
-	  };
-	  console.log("🧾 Usuario seleccionado:", usuario);
-	  setSelectedUsuario(datosMapeados); // ✅ Aquí ahora sí estás enviando lo necesario
+		const datosMapeados = {
+			nombre_usuario: usuario.nombre_usuario,
+			estatus: usuario.estatus ?? "",
+			nombre_rol: usuario.rol?.nombre_rol ?? "",
+		};
+		console.log("🧾 Usuario seleccionado:", usuario);
+		setSelectedUsuario(datosMapeados); // ✅ Aquí ahora sí estás enviando lo necesario
 	};
 
-    const handlePacienteSelect = (paciente) => {
-        setSelectedPaciente(paciente);
-        fetchFamiliar(paciente.identificacion, setSelectedFamiliar);
+	const handlePacienteSelect = (paciente) => {
+		setSelectedPaciente(paciente);
+		fetchFamiliar(paciente.identificacion, setSelectedFamiliar);
 		fetchInfoMilitar(paciente.identificacion, setSelectedInfoMilitar);
 		fetchResidencia(paciente.identificacion, setSelectedResidencia);
 		fetchSeguro(paciente.identificacion, setSelectedSeguro);
-        onOpenChange(false);
-    };
+		onOpenChange(false);
+	};
 
-    const buttons = [
+	const buttons = [
 		{ label: "Buscar Paciente", icon: FaSearch, action: "buscar", color: "bg-gray-400", textColor: "text-black", hoverEffect: "hover:bg-gray-200 hover:text-gray-700", onClick: onOpen },
-        { label: "Cancelar", icon: FaTimes, action: "cancelar", color: "bg-gray-400", textColor: "text-black", hoverEffect: "hover:bg-gray-200 hover:text-gray-700", href: "/admin-dashboard" },
-        { label: "Salir", icon: FaSignOutAlt, action: "salir", color: "bg-gray-400", textColor: "text-black", hoverEffect: "hover:bg-gray-200 hover:text-gray-700", href: "/auth/login" },
-    ];
+		{ label: "Cancelar", icon: FaTimes, action: "cancelar", color: "bg-gray-400", textColor: "text-black", hoverEffect: "hover:bg-gray-200 hover:text-gray-700", href: "/admin-dashboard" },
+		{ label: "Salir", icon: FaSignOutAlt, action: "salir", color: "bg-gray-400", textColor: "text-black", hoverEffect: "hover:bg-gray-200 hover:text-gray-700", href: "/auth/login" },
+	];
 
-   const tabsConfig = [
-	  {
-		key: "actualizar-paciente",
-		title: "1. Paciente",
-		content: (
-		  <ActualizarPaciente pacienteData={selectedPaciente} />
-		),
-	  },
-	  {
-		key: "actualizar-familiar",
-		title: "2. Familiar",
-		content: (
-		  <ActualizarFamiliar familiarData={selectedFamiliar} />
-		),
-	  },
-	  {
-		key: "informacion-militar",
-		title: "3. Información Militar",
-		content: (
-		  <ActualizarInfoMilitar infoMilitarData={selectedInfoMilitar} />
-		),
-	  },
-	  {
-		key: "residencia",
-		title: "4. Residencia",
-		content: (
-		  <ActualizarResidencia residenciaData={selectedResidencia} />
-		),
-	  },
-	  {
-		key: "seguro",
-		title: "5. Seguro",
-		content: (
-		  <ActualizarSeguro seguroData={selectedSeguro} />
-		),
-	  },
-	   {
+	const tabsConfig = [
+		{
+			key: "actualizar-paciente",
+			title: "1. Paciente",
+			content: (
+				<ActualizarPaciente pacienteData={selectedPaciente} />
+			),
+		},
+		{
+			key: "actualizar-familiar",
+			title: "2. Familiar",
+			content: (
+				<ActualizarFamiliar familiarData={selectedFamiliar} />
+			),
+		},
+		{
+			key: "informacion-militar",
+			title: "3. Información Militar",
+			content: (
+				<ActualizarInfoMilitar infoMilitarData={selectedInfoMilitar} />
+			),
+		},
+		{
+			key: "residencia",
+			title: "4. Residencia",
+			content: (
+				<ActualizarResidencia residenciaData={selectedResidencia} />
+			),
+		},
+		{
+			key: "seguro",
+			title: "5. Seguro",
+			content: (
+				<ActualizarSeguro seguroData={selectedSeguro} />
+			),
+		},
+		{
 			key: "usuario",
 			title: "Actualizar Usuario",
 			content: (
-			  <div className="flex flex-col lg:flex-row gap-6">
-				<div className="lg:w-1/2">
-				  <UsuarioSearch onSelectUsuario={handleUsuarioSelect} />
+				<div className="flex flex-col lg:flex-row gap-6">
+					<div className="lg:w-1/2">
+						<UsuarioSearch onSelectUsuario={handleUsuarioSelect} />
+					</div>
+					<div className="lg:w-1/2">
+						<ActualizarEstatusUsuario estatusUsuarioData={selectedUsuario} />
+					</div>
 				</div>
-				<div className="lg:w-1/2">
-				  <ActualizarEstatusUsuario estatusUsuarioData={selectedUsuario} />
-				</div>
-			  </div>
 			),
-		  },
+		},
 	];
 
 
-    return (
-        <div className="bg-white">
-            <NavbarComponent title="Actualizar Paciente y Usuario" buttons={buttons} onAction={(action) => {
-                if (action === "buscar") onOpen();
-                else console.log(action);
-            }} />
+	return (
+		<div className="bg-white">
+			<NavbarComponent title="Actualizar Paciente y Usuario" buttons={buttons} onAction={(action) => {
+				if (action === "buscar") onOpen();
+				else console.log(action);
+			}} />
 
-            <CustomTabs tabs={tabsConfig} />
+			<CustomTabs tabs={tabsConfig} />
 
-            <ReusableModal isOpen={isOpen} onOpenChange={onOpenChange}>
-                <PacienteSearch onSelectPaciente={handlePacienteSelect} />
-            </ReusableModal>
+			<ReusableModal isOpen={isOpen} onOpenChange={onOpenChange}>
+				<PacienteSearch onSelectPaciente={handlePacienteSelect} />
+			</ReusableModal>
 
-        </div>
-    );
+		</div>
+	);
 }
 
