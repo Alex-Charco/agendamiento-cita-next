3de64c93fdbfa8c5bada118be0e8a6b52c7b25fd
Alex-Charco <awcharco@utpl.edu.ec>
2025-04-20 13:04:24 -0500

    Eliminación de código sin uso

--- src/app/utils/api/horarioApi.js
+++ src/app/utils/api/horarioApi.js
@@ -1,16 +1,5 @@
 import authAxios from "@/utils/api/authAxios"; 
 
-// Buscar horario 
-// Aun no esta en uso
-export async function getHorario(query) {
-    if (!query.trim()) return null;
-    
-    const response = await authAxios.get(`/api/horario/get/${query}`);
-	console.log("Response: ", response);
-    console.log("Data: ", response.data);
-    return response.data?.horarios || null;
-}
-
 // *Registrar horario
 export const RegistrarHorario = async (data, identificacion, setMensaje, setSuccess) => {
     try {
